1a7ffa0ff4b611ef71954638e64614cfec5c8180
Andrew Napier <andrew.napier@gologic.com.au>
2023-10-10 21:08:23 +1000

    Halfway through tutorial 1b

--- FluxorLearning/App.cs
+++ FluxorLearning/App.cs
@@ -1,4 +1,5 @@
 ﻿using Fluxor;
+using FluxorLearning.Shared;
 using FluxorLearning.Store;
 using FluxorLearning.Store.CounterUseCase;
 
@@ -7,13 +8,37 @@ public class App
     private readonly IStore _store;
     private readonly IState<CounterState> _counterState;
     private readonly IDispatcher _dispatcher;
+    private readonly IState<WeatherState> _weatherState;
 
-    public App(IStore store, IState<CounterState> counterState, IDispatcher dispatcher)
+    public App(IStore store, IState<CounterState> counterState, IDispatcher dispatcher,
+        IState<WeatherState> weatherState)
     {
         _store = store;
         _counterState = counterState;
         _dispatcher = dispatcher;
+        _weatherState = weatherState;
         _counterState.StateChanged += CounterState_StateChanged;
+        _weatherState.StateChanged += WeatherState_StateChanged;
+    }
+
+    private void WeatherState_StateChanged(object? sender, EventArgs e)
+    {
+        Console.WriteLine("");
+        Console.WriteLine("=========================> WeatherState");
+        Console.WriteLine("IsLoading: " + _weatherState.Value.IsLoading);
+        if (!_weatherState.Value.Forecasts.Any())
+        {
+            Console.WriteLine("--- No weather forecasts");
+        }
+        else
+        {
+            Console.WriteLine("Temp C\tTemp F\tSummary");
+            foreach (WeatherForecast forecast in _weatherState.Value.Forecasts)
+                Console.WriteLine($"{forecast.TemperatureC}\t{forecast.TemperatureF}\t{forecast.Summary}");
+        }
+
+        Console.WriteLine("<========================== WeatherState");
+        Console.WriteLine("");
     }
 
     private void CounterState_StateChanged(object? sender, EventArgs e)
---
+++ FluxorLearning/Store/FetchDataAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluxorLearning.Store
+{
+    internal class FetchDataAction
+    {
+        // TODO: https://github.com/mrpmorris/Fluxor/tree/master/Source/Tutorials/01-BasicConcepts/01B-EffectsTutorial 
+        // continue from "Using an Action and a reducer to alter state"
+    }
+}
